c20d9422ae801344bf13229d97973ae57b4f38b9
ohidulalam <mdnoyonctg15@gmail.com>
2022-08-28 21:47:00 +0600

    File analiysis

---
+++ Client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>You are looking for a javascript programmer</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/pages/HeroBar/HeroBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeroBar.css";
+
+const HeroBar = () => {
+  return (
+    <div>
+      <h1>HeroBar</h1>
+    </div>
+  );
+};
+
+export default HeroBar;
